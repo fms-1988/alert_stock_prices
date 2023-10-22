e728ff4372e272374c51cfce57bf0cf5d3216271
FELIPE MORELLI <43762575+fms-1988@users.noreply.github.com>
2023-10-22 18:35:18 -0300

    Update code1.py

--- code1.py
+++ code1.py
@@ -18,6 +18,25 @@ for ticker, target in zip(tickers, target_prices):
     if last_price < target:
         prices_below_target = True
 
+# Save the stock data to alert.html if any price is below target
+if prices_below_target:
+    file_path = os.path.abspath("data/")
+    alert_file = os.path.join(file_path, "alert.html")
+    
+    with open(alert_file, 'w') as file:
+        # Write beginning of HTML content and table headers
+        file.write("<html><head><style>table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;}</style></head><body>")
+        file.write("<table>")
+        file.write("<tr><th>Ticker</th><th>Last Price</th></tr>")
+        
+        # Write stock data
+        for data in stock_data:
+            file.write(f"<tr><td>{data[0]}</td><td>${data[1]:.2f}</td></tr>")
+        
+        # End table and HTML content
+        file.write("</table></body></html>")
+
+'''
 # Save the stock data to alert.md if any price is below target
 if prices_below_target:
     file_path = os.path.abspath("data/")
@@ -31,6 +50,7 @@ if prices_below_target:
         # Write stock data
         for data in stock_data:
             file.write(f"| {data[0]} | ${data[1]:.2f} |\n")
+'''
 
 '''
 #send email using AWS SES SMTP (this code works well)
