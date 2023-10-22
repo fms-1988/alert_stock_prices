df1612d9be2f51ba05581b429a58b54a71d32530
FELIPE MORELLI <43762575+fms-1988@users.noreply.github.com>
2023-10-22 19:08:11 -0300

    Update code1.py

--- code1.py
+++ code1.py
@@ -37,32 +37,12 @@ if prices_below_target:
             target = target_prices[tickers.index(ticker)]
     
             # Check if the last price is less than the target price, if true set the background color to green
-            bgcolor = "background-color: green;" if last_price < target else ""
+            bgcolor = "background-color: lightgreen;" if last_price < target else ""
             file.write(f"<tr><td>{ticker}</td><td style='{bgcolor}'>${last_price:.2f}</td></tr>")
         
         # End table and HTML content
         file.write("</table></body></html>")
 
-
-'''
-# Save the stock data to alert.html if any price is below target
-if prices_below_target:
-    file_path = os.path.abspath("data/")
-    alert_file = os.path.join(file_path, "alert.html")
-    
-    with open(alert_file, 'w') as file:
-        # Write beginning of HTML content and table headers
-        file.write("<html><head><style>table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;}</style></head><body>")
-        file.write("<table>")
-        file.write("<tr><th>Ticker</th><th>Last Price</th></tr>")
-        
-        # Write stock data
-        for data in stock_data:
-            file.write(f"<tr><td>{data[0]}</td><td>${data[1]:.2f}</td></tr>")
-        
-        # End table and HTML content
-        file.write("</table></body></html>")
-'''
 
 '''
 # Save the stock data to alert.md if any price is below target
