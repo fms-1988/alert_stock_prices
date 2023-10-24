69446a220612db4c1367f8ae469a86fab137b79a
FELIPE MORELLI <43762575+fms-1988@users.noreply.github.com>
2023-10-24 17:04:27 -0300

    Update code1.py

--- code1.py
+++ code1.py
@@ -6,8 +6,8 @@ def get_last_price(ticker_symbol):
     todays_data = stock.history(period="1d")
     return todays_data['Close'].iloc[0]
 
-tickers = ["AAPL", "MSFT"]
-target_prices = [2000, 2000]  # Example target prices for AAPL and MSFT
+tickers = ["CSMG3.SA", "PETR3.SA", "USIM5.SA"]
+target_prices = [14, 25, 6]
 
 
 # Store prices and check if any are below target
